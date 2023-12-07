bca2719145e0c751cd47dddd5c0ad04a93355fd0
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-12-07 12:41:14 +0200

    task5 fix2

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Complex Module """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sum function """
     total: float = 0
     for number in input_list:
