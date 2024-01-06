7cc342737d855db2e5530aca6ea84c46d914ac6f
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-06 14:17:20 +0200

    task4 fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized
-from utils import get_json
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """ Test github org client """
@@ -16,7 +16,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, org_name, mock_get_json):
-        """ Test org """
+        """ Test org method """
         org_object = GithubOrgClient(org_name)
         org_object.org()
         mock_get_json.assert_called_once()
